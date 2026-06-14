9066d14f451693aa232b795bce5ed30ce1903dbb
d1mamba <dimambaofficial@gmail.com>
2026-06-14 12:20:21 +0300

    Update key.js

--- api/key.js
+++ api/key.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.json({ key: process.env.GEMINI_API_KEY });
-}
+};
