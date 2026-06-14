cbe8f4b761b528baf2deb9c1381107c1f14e4e39
d1mamba <dimambaofficial@gmail.com>
2026-06-14 08:58:39 +0300

    Update getKey.js

--- api/getKey.js
+++ api/getKey.js
@@ -1,14 +1,1 @@
-export default function handler(req, res) {
-    const apiKey = process.env.GEMINI_API_KEY;
-    
-    if (!apiKey) {
-        return res.status(500).json({ 
-            error: 'API key not configured',
-            key: null 
-        });
-    }
-    
-    res.status(200).json({ 
-        key: apiKey 
-    });
-}
+
